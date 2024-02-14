f95f6d4ccf89073ccbcfdb12e4345188ea11629a
ramakrishnade <ramky.drk@gmail.com>
2024-02-14 18:02:02 +0530

    Update sayHello.groovy

--- vars/sayHello.groovy
+++ vars/sayHello.groovy
@@ -2,6 +2,6 @@ def call()
 {
    echo 'Hello shared library'
    def username = 'Jenkins'
-   echo 'Hello Mr. ${username}'
+   echo "Hello Mr. ${username}"
    echo "I said, Hello Mr. ${username}"
 }
