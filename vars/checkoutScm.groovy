6d8be5f0c980da67338b8cef03481ebb1a65e4e3
ramakrishnade <ramky.drk@gmail.com>
2024-02-15 11:13:02 +0530

    Update checkoutScm.groovy

--- vars/checkoutScm.groovy
+++ vars/checkoutScm.groovy
@@ -1,7 +1,7 @@
 /**
  * Helper function to checkout a branch
  * @param  branch     the branch name
- * @param  repository the targted repository name
+ * @param  repoUrl the targted repository name
  */
 def call(Map gitParams)
 {
