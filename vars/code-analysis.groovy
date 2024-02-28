465a9b2299b5ab7b095185bca6c1f1881d45c9cc
ramakrishnade <ramky.drk@gmail.com>
2024-02-28 15:19:58 +0530

    code analysis added

--- vars/code-analysis.groovy
+++ vars/code-analysis.groovy
@@ -1,6 +1,7 @@
 def(Map sonarParams)
 {
     withSonarQubeEnv("${sonarParams.SonarQube}") {
+        echo "${sonarParams.SonarQube}.${sonarParams.scannerHome}"
         sh "${sonarParams.scannerHome}/bin/sonar-scanner"
     }
 
